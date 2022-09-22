387954d2081b99c9647214b2bdf0620e528cbcb0
김정일 <0rlawjddlf0@naver.com>
2022-09-22 14:49:30 +0000

    22.09.22.

--- problem/Score.py
+++ problem/Score.py
@@ -1,6 +1,6 @@
 #ox 퀴즈
 
-
+'''
 n = int(input())
 
 for _ in range(n):
@@ -15,4 +15,43 @@ for _ in range(n):
             sum += j
         score += sum
     print(score)
+'''
 
+'''
+N = int(input())
+
+count = 0
+
+while count < N:
+    n = str(input())
+    o = n.split('X')
+    score = 0
+    for i in o:
+        sum = 0
+        j = 0
+        while j <= len(i):
+            sum += j
+            j += 1
+        score += sum
+    print(score)
+    count += 1
+'''
+
+'''
+#단어의 개수
+n = str(input())
+
+s = n.split(' ')
+
+if s[0] == '':
+    s.remove('')
+if s[-1] == '':
+    s.remove('')
+
+count = 0
+    
+for i in s:
+        count += 1
+    
+print(count)
+'''
