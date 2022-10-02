a1b83fb167476bdbe80771f5eeea9c3769454aa7
김정일 <0rlawjddlf0@naver.com>
2022-10-02 05:04:19 +0000

    22.10.02.

--- problem/To sort numbers.py
+++ problem/To sort numbers.py
@@ -1,35 +1,29 @@
 #수 정렬하기 3
 #선택, 삽입, 버블
-'''
-n = int(input())
+import sys
 
-cnt = []
+n = int(sys.stdin.readline())
+num_list = [0] * 10001
 
-for _ in range(n):
-    i = int(input())
-    cnt.append(i) # cnt = [5,2,3,1,4,2,3,5,1,7]
+for _ in range(n): # 크기 10001의 리스트에 숫자가 들어오면 그 숫자를 리스트인덱스에 맞게 더해줌!!
+    num_list[int(sys.stdin.readline())] += 1
+    
+for i in range(10001):
+    if num_list != 0: 
+        for j in range(num_list[i]): # 리스트 인덱스에 숫자가 있는만큼 
+            print(i)
 
-
-for i in range(len(cnt)):
-    for j in range(len(cnt)- i - 1):
-        if cnt[j] > cnt[j+1]:
-            tmp = cnt[j]
-            cnt[j] = cnt[j+1]
-            cnt[j+1] = tmp
-
-for i in cnt:
-    print(i)
-'''
     
 #수 정렬하기 1
 #bubble sort
+'''
 n = int(input())
 
 cnt = []
 
 for _ in range(n):
     i = int(input())
-    cnt.append(i) # cnt = [5,2,3,1,4,2,3,5,1,7]
+    cnt.append(i)
 
 
 for i in range(len(cnt)):
@@ -41,3 +35,21 @@ for i in range(len(cnt)):
 
 for i in cnt:
     print(i)
+'''
+
+#수 정렬하기 2
+#input이 아닌 sys의 sys.stdin.readline을 사용해서
+'''
+import sys
+n = int(input())
+
+cnt = []
+
+for _ in range(n):
+    cnt.append(int(sys.stdin.readline()))
+    
+cnt.sort()
+
+for i in cnt:
+    print(i)
+'''
