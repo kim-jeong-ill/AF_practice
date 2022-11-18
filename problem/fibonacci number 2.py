4cafb2fe011f6f75402913f6dc898e5f2c2fccd9
김정일 <0rlawjddlf0@naver.com>
2022-11-18 11:27:26 +0000

    22.11.18.

---
+++ problem/fibonacci number 2.py
@@ -0,0 +1,12 @@
+#피보나치 수열2
+
+def dp_fibo(n):
+    dp = [0] * (n+1)
+    dp[0], dp[1] = 0, 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    return dp[n]
+
+m = int(input())
+
+print(dp_fibo(m))
