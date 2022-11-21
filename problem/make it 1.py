6f3be470e888a03a50f1f9a04abfad0827a6b3e9
김정일 <0rlawjddlf0@naver.com>
2022-11-21 11:06:04 +0000

    22.11.21.

--- problem/make it 1.py
+++ problem/make it 1.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 dp = [0] * (n+1)
@@ -7,9 +10,9 @@ for i in range(2, n+1):
 
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i//2]+1)
+        #1을 더하는 것은 d는 결과가 아닌 계산한 횟수를 저장하는 것 이기 때문이다. d[i]에는 더하지 않는 이유는 이미 1을 뺄 때 1을 더해준 이력이 있기 때문이다.
+        #cool~
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i//3] + 1)
 
 print(dp[n])
-
-#유튜브에서 설명 듣고 풀자;;
