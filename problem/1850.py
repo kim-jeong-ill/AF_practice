e697ff1413b427f6bea25b9155faf3c0ceccebe1
김정일 <0rlawjddlf0@naver.com>
2022-11-27 11:07:28 +0000

    22.11.27.

---
+++ problem/1850.py
@@ -0,0 +1,23 @@
+#최대공약수
+
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split()) 
+
+    original_a = a
+    original_b = b
+
+    if b > a:
+        tmp = a
+        a = b
+        b = tmp
+
+    while 1:
+        rest = a % b
+        if rest == 0:
+            break
+        a = b 
+        b = rest # b는 최대공약수
+
+    print(original_a*original_b//b)
--- problem/9095.py
+++ problem/9095.py
@@ -1,13 +1,15 @@
 #1,2,3 더하기
+import sys
+input = sys.stdin.readline
 
 T = int(input())
 
 for _ in range(T):
-    dp = [0] * 12
     n = int(input())
-    dp[0] = 1
-    dp[1] = 2
-    dp[2] = 4
-    for i in range(3, n+1):
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    dp = [0] * 11 # 11보다 작다니까 11로 설정
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    for i in range(4, 11):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
     print(dp[n])
