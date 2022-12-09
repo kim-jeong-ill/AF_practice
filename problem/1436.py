cfeed532cceb8682d65e888fe25e34b86b8b4ba8
김정일 <0rlawjddlf0@naver.com>
2022-12-09 07:52:29 +0000

    22.12.09.

--- problem/1436.py
+++ problem/1436.py
@@ -2,19 +2,12 @@
 
 n = int(input())
 
-count = 1
-k = 665
+count = 0
+k = 650
 while 1:
-    k += 1
-    i = 0
-    while i < len(str(k)):
-        i = str(i)
-        if k[i] == 6 and k[i+1] == 6 and k[i+2] == 6:
-            count += 1
-            break
-        i = int(i)
-    i += 1
+    if '666' in str(k):
+        count += 1
     if count == n:
+        print(k)
         break
-        
-print(k)
+    k += 1
