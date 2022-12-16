61f7cece48af314687167e2a4e15c46f70e08d4e
김정일 <0rlawjddlf0@naver.com>
2022-12-16 13:25:45 +0000

    22.12.16.

---
+++ problem/10768.py
@@ -0,0 +1,17 @@
+#특별한 날
+
+m = int(input())
+
+d = int(input())
+
+if m < 2:
+    print("Before")
+elif m == 2:
+    if d < 18:
+        print("Before")
+    elif d == 18:
+        print("Special")
+    else:
+        print("After")
+else:
+    print("After")
