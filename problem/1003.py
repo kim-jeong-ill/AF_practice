4a8c7f051d5cc8fb137242376a862495522beb6c
김정일 <0rlawjddlf0@naver.com>
2022-12-29 08:16:32 +0000

    22.12.29.

--- problem/1003.py
+++ problem/1003.py
@@ -1,4 +1,7 @@
 #피보나치 함수, 누구미 마렵다
+#https://datazzang.tistory.com/18
+#위에 풀이 멋있네
+'''
 import sys
 input = sys.stdin.readline
 
@@ -24,3 +27,23 @@ for _ in range(T):
         if i == '1':
             count2 += 1
     print(count1, count2)
+'''
+
+T = int(input())
+
+
+cnt0 = [1,0,1]
+cnt1 = [0,1,1]
+
+def fibo(num):
+    length = len(cnt0)
+    if num >= length:
+        for i in range(length, num+1):
+            cnt0.append(cnt0[i-2] + cnt0[i-1])
+            cnt1.append(cnt1[i-2] + cnt1[i-1])
+    print('{} {}'.format(cnt0[num], cnt1[num]))
+
+for _ in range(T):
+    x = int(input())
+    fibo(x)
+    
