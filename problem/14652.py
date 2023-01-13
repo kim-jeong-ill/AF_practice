97a9de930937f4470162af53be206987972bbce0
김정일 <0rlawjddlf0@naver.com>
2023-01-13 08:28:08 +0000

    23.1.13.

--- problem/14652.py
+++ problem/14652.py
@@ -3,3 +3,15 @@
 n,m,k = map(int, input().split()) # n=세로 m=가로 k=관중석번호
 
 print(k//m, k%m)
+
+
+#Cupcake Party - 24568
+a = int(input())
+b = int(input())
+
+result = (a*8 + b*3)
+
+if result <= 28:
+    print(0)
+else:
+    print(result - 28)
