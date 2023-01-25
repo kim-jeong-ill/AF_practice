b2ccb6f54a95b8aed91823a7ce02e93fcd53588f
김정일 <0rlawjddlf0@naver.com>
2023-01-25 12:49:53 +0000

    23.1.25.

--- problem/10162.py
+++ problem/10162.py
@@ -28,3 +28,17 @@ if rest3 == 0:
     print(share1, share2, share3)
 else:
     print(-1)
+    
+'''
+T = int(input())
+
+if T % 10 != 0:
+    print(-1)
+else:
+    A = B = C = 0
+    A = T // 300
+    B = (T % 300) // 60
+    C = (T % 300) % 60 // 10
+    print(A, B, C)
+'''
+print()
