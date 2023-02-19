afdc538ec0fdbf2ace0d5154cb7eff525ec19f6e
김정일 <0rlawjddlf0@naver.com>
2023-02-19 02:08:15 +0000

    23.2.19.

---
+++ problem/2935.py
@@ -0,0 +1,10 @@
+#소음
+
+a = int(input())
+c = str(input())
+b = int(input())
+
+if c == '+':
+    print(a+b)
+else:
+    print(a*b)
--- problem/3053.py
+++ problem/3053.py
@@ -2,8 +2,8 @@
 import math
 
 n = float(input())
-
-print(round(n*n*(math.pi),6))
-print(f'{2*n*n:.6f}')
+ 
+print(round(n*n*(math.pi),6)) # round 함수를 사용해서
+print(f'{2*n*n:.6f}') # f의 fomating을 사용해서
 
 #print(f'{2*r*r:.6f}')
