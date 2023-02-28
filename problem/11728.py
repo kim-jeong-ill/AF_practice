6100e7b180a72794be151a5abd74d0f87757ef96
김정일 <0rlawjddlf0@naver.com>
2023-02-28 11:29:33 +0000

    23.2.28.

---
+++ problem/11728.py
@@ -0,0 +1,15 @@
+#배열 합치기
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+b = list(map(int, input().split()))
+
+c = a + b
+
+c.sort()
+for i in c:
+    print(i, end = ' ')
---
+++ problem/5800.py
@@ -0,0 +1,9 @@
+#성적 통계
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    l = list(map(int, input().split()))
+    
