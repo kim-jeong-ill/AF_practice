5b7286f8367453e27a75437916794132cbec5258
김정일 <0rlawjddlf0@naver.com>
2023-03-11 07:49:50 +0000

    23.3.11.

--- problem/5800.py
+++ problem/5800.py
@@ -4,6 +4,14 @@ input = sys.stdin.readline
 
 n = int(input())
 
-for _ in range(n):
+#Max 78, Min 23, Largest gap 46
+for i in range(n):
     l = list(map(int, input().split()))
-    
+    l = l[1:]
+    l.sort()
+    s = -1
+    for j in range(len(l)-1): # 5
+        if l[j+1] - l[j] > s:
+            s = l[j+1] - l[j]
+    print('Class', i+1)
+    print('Max {0}, Min {1}, Largest gap {2}'.format(max(l), min(l),s))
