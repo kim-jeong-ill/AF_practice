49779c08daddf083b40252855ba55e61d8ccda8a
김정일 <0rlawjddlf0@naver.com>
2023-03-20 05:02:26 +0000

    23.3.20.

--- problem/15873.py
+++ problem/15873.py
@@ -1,9 +1,17 @@
 #공백 없는 A+B
 
-n = list(input())
+n = list(str(input()))
 
-a = int(''.join(n[0:-1]))
+n = list(map(int, n))
 
-b = int(n[-1])
-
-print(a+b)
+if len(n) <= 2:
+    print(n[0] + n[1])
+    
+else:
+    if len(n) == 4:
+        print(20)
+    else:
+        if n[1] == 0:
+            print(10 + n[2])
+        else:
+            print(n[0] + 10)
