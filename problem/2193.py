d95e15e7e9aefae803fabd644886992f72a78b57
김정일 <0rlawjddlf0@naver.com>
2023-04-13 07:30:10 +0000

    23.4.13.

---
+++ problem/2193.py
@@ -0,0 +1,15 @@
+#이친수
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 91
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+    
+print(dp[n])
