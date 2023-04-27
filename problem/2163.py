93ff2f2ba2de9dea95de7a5b527727bd7b092482
김정일 <0rlawjddlf0@naver.com>
2023-04-27 11:36:46 +0000

    23.4.27.

--- problem/2163.py
+++ problem/2163.py
@@ -1,5 +1,20 @@
 #초콜릿 자르기
-
+'''
 n, m = map(int,input().split())
 
 print(n*m-1)
+
+'''
+
+
+#25628
+#햄버거 만들기
+
+a,b = map(int,input().split())
+
+a = a//2
+
+if a <= b:
+    print(a)
+else:
+    print(b)
