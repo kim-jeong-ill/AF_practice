7f07e269448e32615d98fdc7da5c2d0472caa521
김정일 <0rlawjddlf0@naver.com>
2023-05-29 09:58:59 +0000

    23.5.29.

--- problem/10810.py
+++ problem/10810.py
@@ -1,7 +1,12 @@
 #공 넣기
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
+b = [0] * n
 
 for _ in range(m):
-    i,j,k = map(int,input().split())
-    
+    i, j, k = map(int, input().split())
+    for x in range(i, j+1):
+        b[x-1] = k
+        
+for i in range(n):
+    print(b[i], end = ' ')
